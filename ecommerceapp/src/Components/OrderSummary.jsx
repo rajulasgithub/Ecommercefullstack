70db2a746b86ab8df593612920fe1ccc44d196d4
rajulasgithub <rajulasrazak688@gmail.com>
2024-08-22 23:07:29 +0530

    ad

--- ecommerceapp/src/Components/OrderSummary.jsx
+++ ecommerceapp/src/Components/OrderSummary.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import './Style.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 const OrderSummary = () => {
   return (
-    <div>
+    <div className='odrsmryback'>
         <div>
-            
+        <Container>
+      <Row>
+        <Col>
+        <div className='odrmaindiv'>
+          <div>
+      <h3>Orer Total</h3>
+      <h3>...</h3>
+      </div>
+
+        </div>
+        </Col>
+      </Row>
+      </Container>
+
         </div>
     </div>
   )
