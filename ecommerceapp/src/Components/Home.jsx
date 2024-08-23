572209cb48b716fcb0805ae45d4bd6721e8a8dc0
rajulasgithub <rajulasrazak688@gmail.com>
2024-08-23 22:43:46 +0530

    add

--- ecommerceapp/src/Components/Home.jsx
+++ ecommerceapp/src/Components/Home.jsx
@@ -8,16 +8,7 @@ const Home = () => {
   return (
     <div>
         <div >
-        <Container>
-      <Row >
-        <Col className='homecolone'>1 of 2</Col>
-        <Col>2 of 2</Col>   
-      </Row>
-      <Row >
-        <Col className='homecoltwo'>1 of 2</Col>
-        <Col>2 of 2</Col>   
-      </Row>
-      </Container>
+        
 
         </div>
 
